aad91fcdb72d6a64df2998fb2bdd067cff53d1b2
JAROFGORM <powwy.pw@gmail.com>
2020-09-01 04:40:38 -0400

    commit #2
    Added command handler, and installed EsLint to help us out.

--- mc.js
+++ mc.js
@@ -1,11 +1,41 @@
 const Discord = require('discord.js');
+const config =  require('./config.json');
+const client = new Discord.Client({disableEveryone: true});
+const fs = require('fs');
+client.commands = new Discord.Collection();
 
-const Client = new Discord.Client;
+fs.readdir("./commands/", (err, files) => {
+    if (err) console.log(err);
 
-Client.once('ready', async () => {
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if(jsfile.length <= 0){
+        console.log("Error: command not found, sorry pal.")
+        return;
+    }
+
+    jsfile.forEach((f, i) => {
+        let cog = require(`./commands/${f}`)
+            console.log(`${f} loaded!`)
+            client.commands.set(cog.help.name, cog);
+    })
+});
+
+client.once('ready', async () => {
     console.log(`${bot.user.username} is online in ${guild.name}`)
     bot.user.setActivity("ClimbMC.gg", {type: "PLAYING"});
 
 });
 
-Client.login(config.token);
+client.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let handle = config.handle;
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+    let commandfile = bot.commands.get(command.slice(handle.length));
+        if(commandFile) commandFile.run(client, message, args);
+
+})
+
+client.login(config.token);
